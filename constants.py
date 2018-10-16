b66bf1dacdd81a47f0977fdb1e1ad7e1c85a8da2
Aviel Fedida <avielfedida@gmail.com>
2018-10-16 13:19:31 +0300

    Raise SELF_PACE_MODE_POOL to 80

--- constants.py
+++ constants.py
@@ -5,6 +5,6 @@ DEFAULT_CATEGORY = 'General'
 AVAILABLE_MODES = [MODE_SELF_PACE, MODE_SPEED]
 
 # Configurable attributes
-SELF_PACE_MODE_POOL = 30  # How many records are retrieved in order to choose 1(depends on ratios)
+SELF_PACE_MODE_POOL = 80  # How many records are retrieved in order to choose 1(depends on ratios)
 SPEED_MODE_LIMIT = 50
 DEFAULT_STATS_DAYS = 30
